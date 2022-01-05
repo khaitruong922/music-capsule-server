a2a848fe0d166a576bf818ab5e1d2aac627a2e3d
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2022-01-06 01:39:46 +0700

    Revert "Revert "not change speed when change pitch""
    This reverts commit 65127c6d4781a35a870c433e64b76f362c0d98a0.

--- src/core/downloader/downloader.service.ts
+++ src/core/downloader/downloader.service.ts
@@ -93,9 +93,11 @@ export class DownloaderService {
     const outputFileName = `${name}_${semitoneShift}_x${playbackSpeed}${ext}`;
     const outputFilePath = path.join(dir, outputFileName);
     const SAMPLE_RATE = 48000;
-    const hz = SAMPLE_RATE * Math.pow(2, semitoneShift / 12);
+    const modifier = Math.pow(2, semitoneShift / 12);
+    const hz = SAMPLE_RATE * modifier;
+    const tempo = playbackSpeed / modifier;
     console.log(hz);
-    const command = `ffmpeg -y -i ${filePath} -af asetrate=${hz},aresample=${SAMPLE_RATE},atempo=${playbackSpeed} ${outputFilePath}`;
+    const command = `ffmpeg -y -i ${filePath} -af asetrate=${hz},aresample=${SAMPLE_RATE},atempo=${tempo} ${outputFilePath}`;
     await execAsync(command);
     console.log(command);
     return outputFilePath;
