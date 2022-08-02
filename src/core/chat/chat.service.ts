321362d52a6270e7955ee005a4c293377701c104
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2022-08-02 16:42:22 +0700

    Update chat.service.ts

--- src/core/chat/chat.service.ts
+++ src/core/chat/chat.service.ts
@@ -9,7 +9,7 @@ export class ChatService {
         if (message.startsWith(this.COMMAND_PREFIX)) {
             if (message.startsWith(`${this.COMMAND_PREFIX}ff`)) {
                 const seconds = parseInt(message.split(" ")[1], 10)
-                if (Number.isNaN(seconds)) {
+                if (Number.isNaN(seconds) || seconds <= 0) {
                     throw new InvalidCommand(
                         "Fast forward failed - Invalid number",
                     )
